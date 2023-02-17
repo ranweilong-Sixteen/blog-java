529c7e625bd5cc932ec9a0a39711e5a80491474c
ranweilong <1683361020@qq.com>
2023-02-17 16:07:43 +0800

    添加

--- src/main/java/com/rwlok/blogjava/BlogJavaApplication.java
+++ src/main/java/com/rwlok/blogjava/BlogJavaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogJavaApplication {
-
+  // 启动类
   public static void main(String[] args) {
     SpringApplication.run(BlogJavaApplication.class, args);
   }
