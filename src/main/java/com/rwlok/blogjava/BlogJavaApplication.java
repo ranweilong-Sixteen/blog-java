1308a10591ef06dfaa0c803eeddd890b4f27639f
ranweilong <1683361020@qq.com>
2023-02-17 15:58:25 +0800

    项目初始化

---
+++ src/main/java/com/rwlok/blogjava/BlogJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rwlok.blogjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BlogJavaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/rwlok/blogjava/BlogJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rwlok.blogjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
